ad109939c93f4360b2f16100ce4593dae039dd73
SabrinaVieira <sah.luciana@gmail.com>
2021-08-20 01:29:16 -0300

    :construction: Added typescript,sass, styles are using css module, _document tsx created

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <>
+        <Head>
+          <title>Inicio | ig.news</title>
+        </Head>
+        <h1>Hello</h1>
+      </>
+    </div>
+  );
+}
