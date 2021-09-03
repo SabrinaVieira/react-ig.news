ca38c2ef3b263fe94ea7f88fbf64346dddb60467
SabrinaVieira <sah.luciana@gmail.com>
2021-09-03 20:43:14 -0300

    :contruction: checkpoint: Setup Stripe API

--- src/components/SingInButton/index.tsx
+++ src/components/SingInButton/index.tsx
@@ -1,25 +1,19 @@
-//import { FaGithub } from "react-icons/fa";
-//import { FiX } from "react-icons/fi";
-import Image from "next/image";
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
 import styles from "./styles.module.scss";
-import githubicon from "../../../public/githubicon.png";
 
 export function SingInButton() {
   const isUserLoggedIn = true;
 
   return isUserLoggedIn ? (
     <button type="button" className={styles.sigInButton}>
-      {/* <Image src={githubicon} alt="github icon" className={styles.image} /> */}
-      <img src="/githubicon.png" alt="github icon" s />
-      {/* <FaGithub color="04d361"/> */}
-      {/* <FiX className={styles.closeIcon} color="#7377380" /> */}
+      <FaGithub color="04d361" />
       Sabrina Vieira
+      <FiX color="#7377380" className={styles.closeIcon} />
     </button>
   ) : (
     <button type="button" className={styles.sigInButton}>
-      {/* <Image src={githubicon} alt="github icon" className={styles.image} /> */}
-      <img src="/githubicon.png" alt="github icon" s />
-      {/* <FaGithub color="eba417"/> */}
+      <FaGithub color="eba417" />
       Sing in with github
     </button>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,33 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 import styles from "./home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+};
+
+export default function Home({product}: HomeProps) {
   return (
     <div>
       <>
         <Head>
           <title>Home | ig.news</title>
         </Head>
+
         <main className={styles.contentContainer}>
           <section>
-            <span>Hey Welcome</span>
+            <span>👏 Hey Welcome</span>
             <h1>
-              News about <span>world.</span>
+              News about <span>React</span> world.
             </h1>
             <p>
-              get acss to all the publications
-              <br />
-              <span>for $ 9.90 per month</span>
+              get access to all the publications <br />
+              <span>for {product.amount} per month</span>
             </p>
             <SubscribeButton />
           </section>
@@ -31,4 +40,19 @@ export default function Home() {
       </>
     </div>
   );
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1JU0nvIf5W7wkdGbAcSMMuMR",{expand: ["product"]})
+
+  const product = {
+    priceId: price.id,
+    amount: price.unit_amount / 100,
+  };
+
+  return(
+    props: {
+      product,
+    }
+  )
 }
